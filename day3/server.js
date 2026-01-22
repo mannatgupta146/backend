3f1fb1d4df5400d3eb28fce674353fee9eb39b2a
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-23 02:53:35 +0530

    api learing

--- day3/server.js
+++ day3/server.js
@@ -2,8 +2,19 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req,res) => {
-    res.send('heelo')
+let notes = []
+
+app.use(express.json())
+
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+    
+    notes.push(req.body)
+    res.send('note added')
+})
+
+app.get('/notes', (req,res) => {
+    res.send(notes)
 })
 
 app.listen(3000)
