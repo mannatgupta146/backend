80100f813408f57f890061616e4503bfddc94ce9
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-24 23:30:05 +0530

    learned completly

--- day4/server.js
+++ day4/server.js
@@ -5,24 +5,3 @@ app.listen(3000, ()=>{
     
 })
 
-let notes = []
-
-app.use(express.json())
-
-app.post('/notes', (req, res)){
-    notes.push(req.body)    
-    res.send('note added')
-    console.log(req.body);
-}
-
-app.get('/notes', (req, res)){
-    res.send(notes)
-}
-
-app.delete('/notes', (req, res)){
-    
-}
-
-app.patch('/notes', (req, res)){
-    
-}
--- day4/src/app.js
+++ day4/src/app.js
@@ -2,4 +2,33 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+let notes = []
+
+app.post('/notes', (req, res)=>{
+    notes.push(req.body)    
+    res.send('note added')
+    console.log(req.body);
+})
+
+app.get('/notes', (req, res)=>{
+    res.send(notes)
+})
+
+app.delete('/notes/:index', (req, res)=>{
+    delete notes[req.params.index]
+    res.send('note deleted')
+})
+
+app.patch('/notes/:index', (req, res)=>{
+    notes[req.params.index].title = req.body.title
+    res.send('note updated')
+})
+
+app.put('/notes/:index', (req, res)=>{
+    notes[req.params.index]= req.body
+    res.send('note updated')
+})
+
 module.exports = app
