77db8dd8ea9840d7f9377cf59e357c168aae69a9
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-26 04:02:46 +0530

    connected

--- day5/src/app.js
+++ day5/src/app.js
@@ -4,4 +4,40 @@ const app = express()
 
 app.use(express.json())
 
+let notes = []
+
+app.get('/notes',(req, res)=>{
+    res.status(200).json({
+        message: notes
+    })
+})
+
+app.post('/notes',(req, res)=>{
+    notes.push(req.body)
+    res.status(201).json({
+        message: 'notes added'
+    })
+})
+
+app.delete('/notes/:index',(req, res)=>{
+    delete notes[req.params.index]
+    res.status(200).json({
+        message: 'deleted'
+    })
+})
+
+app.put('/notes/:index',(req, res)=>{
+    notes[req.params.index] = req.body
+    res.status(200).json({
+        message: 'modified'
+    })
+})
+
+app.patch('/notes/:index',(req, res)=>{
+    notes[req.params.index] = req.body.description
+    res.status(200).json({
+        message: 'modified'
+    })
+})
+
 module.exports = app
