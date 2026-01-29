7edd2917da2ac1ba7b83633898b97b3167455832
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-29 17:08:41 +0530

    mongodb connected

--- day6/server.js
+++ day6/server.js
@@ -1,4 +1,15 @@
 const app = require('./src/app')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+function connectToDb(){
+    mongoose.connect(`${process.env.MONGODB_URI}/day-6`)
+    .then(()=> {
+        console.log('connected to database');       
+    })
+}
+connectToDb()
 
 app.listen(3000,(req, res)=> {
     console.log('app is running on port 3000');
