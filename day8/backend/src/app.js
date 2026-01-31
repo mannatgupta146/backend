5425bed2021e77c2287e097887a524cd958a96c1
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-01 00:56:35 +0530

    done

--- day8/backend/src/app.js
+++ day8/backend/src/app.js
@@ -1,7 +1,49 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const notesModel = require('../models/notes.model.js')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+app.post('/api/notes', async(req, res) => {
+    const {title, description} = req.body
+
+    const note = await notesModel.create({title, description})
+    res.status(201).json({
+        message: "note created"
+    })
+})
+
+app.get('/api/notes', async(req, res) => {
+    const notes = await notesModel.find()
+    res.status(200).json({
+        message: "all notes",
+        notes
+    })
+})
+
+app.patch('/api/notes/:id', async(req, res) => {
+    const {id} = req.params
+    const {description} = req.body
+
+    await notesModel.findByIdAndUpdate(id, {description})
+
+    res.status(200).json({
+        message: 'note updated'
+    })
+})
+
+app.delete('/api/notes/:id', async(req, res) => {
+    const {id} = req.params
+
+    await notesModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: 'note deleted'
+    })
+})
 
 module.exports = app
