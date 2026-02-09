5790b71fe85f272265fa557cff4700268b2c37f4
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-10 01:02:34 +0530

    register api

---
+++ day15/routes/auth.route.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+const userModel = require('../models/user.model')
+
+const authRouter = express.Router()
+
+authRouter.post('/register', async(req, res) =>{
+    const {name, email, password} = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({email})
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message: "User already exists"
+        })
+    }
+
+    const hash = crypto.createHash('md5').update(password).digest('hex')
+
+    const user = await userModel.create({name, email, password : hash})
+
+    const token = jwt.sign(
+        {
+            id: user._id
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie('jwt_token', token)
+
+    res.status(201).json({
+        message: "user registered successfully"
+    })
+})
+
+module.exports = authRouter
--- day15/src/app.js
+++ day15/src/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const authRouter = require('../routes/auth.route')
 
 const app = express()
+app.use(cookieParser())
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 module.exports = app
