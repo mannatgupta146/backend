96bd5892612a381aad79312ba2d8254cb0bb752a
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-08 23:51:05 +0530

    app running on port 3000

--- google-auth/server.js
+++ google-auth/server.js
@@ -1,3 +1,10 @@
 import express from 'express';
+import {config} from 'dotenv';
+
+config();
 
 const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
