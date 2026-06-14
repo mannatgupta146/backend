caeac6cd97fb1b4f18b07f38169947a76ff39d08
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-14 13:30:23 +0530

    feat: implement user registration and login services with JWT access and refresh token cookie handling

--- access-refresh-token/src/controllers/auth.controller.js
+++ access-refresh-token/src/controllers/auth.controller.js
@@ -1,49 +1,47 @@
-import { loginService, registerService } from "../services/auth.service.js";
+import { loginService, registerService } from "../services/auth.service.js"
 
 export const registerController = async (req, res) => {
-    
-    let { accessToken, refreshToken, newUser } = await registerService(req.body)
+  let { accessToken, refreshToken, newUser } = await registerService(req.body)
 
-    res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        sameSite: "lax",
-        secure: false,
-        maxAge: 10 * 60 * 1000 // 10 min
-    })
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000 // 1 day
-    }) 
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 10 * 60 * 1000, // 10 min
+  })
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  })
 
-    return res.status(201).json({
-        message: "User created successfully",
-        newUser
-    })
-
+  return res.status(201).json({
+    message: "User created successfully",
+    newUser,
+  })
 }
 
 export const loginController = async (req, res) => {
-    let { accessToken, refreshToken, user } = await loginService(req.body)
+  let { accessToken, refreshToken, user } = await loginService(req.body)
 
-    res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        sameSite: "lax",
-        secure: false,
-        maxAge: 10 * 60 * 1000 // 10 min
-    })
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000 // 1 day
-    }) 
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 10 * 60 * 1000, // 10 min
+  })
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  })
 
-    return res.status(200).json({
-        message: "User logged in successfully",
-        user
-    })
+  return res.status(200).json({
+    message: "User logged in successfully",
+    user,
+  })
 }
 
 export default { registerController, loginController }
--- access-refresh-token/src/services/auth.service.js
+++ access-refresh-token/src/services/auth.service.js
@@ -1,73 +1,78 @@
-import userModel from "../models/user.model.js";
+import userModel from "../models/user.model.js"
 import bcrypt from "bcrypt"
-import { generateAccessToken } from "../utils/generateToken.js";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/generateToken.js"
 
 export const registerService = async (data) => {
-    try {
-        let { email, password, name } = data
+  try {
+    let { email, password, name } = data
 
-        if (!email || !password || !name) {
-                return res.status(400).json({ message: "All the fields are required" })
-            }
-        
-            // Checking user exists
-            const userExists = await userModel.findOne({ email });
-        
-            if (userExists) {
-                return res.status(409).json({ message: "User already exists" })
-            }
-        
-            let hasPassword = bcrypt.hashSync(password, 10)
-        
-            let newUser = await userModel.create({
-                name,
-                email,
-                password: hasPassword
-            })
-        
-            let accessToken = generateAccessToken(newUser._id)
-            let refreshToken = generateRefreshToken(newUser._id)
-
-            return {
-                accessToken, refreshToken, newUser
-            }
-
-    } catch (error) {
-        throw new Error("Internal Server Error")
+    if (!email || !password || !name) {
+        throw new Error("All fields are required")
     }
+
+    // Checking user exists
+    const userExists = await userModel.findOne({ email })
+
+    if (userExists) {
+      throw new Error("User already exists")
+    }
+
+    let hasPassword = bcrypt.hashSync(password, 10)
+
+    let newUser = await userModel.create({
+      name,
+      email,
+      password: hasPassword,
+    })
+
+    let accessToken = generateAccessToken(newUser._id)
+    let refreshToken = generateRefreshToken(newUser._id)
+
+    return {
+      accessToken,
+      refreshToken,
+      newUser,
+    }
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 export const loginService = async (data) => {
-    try {
-        let { email, password} = data
+  try {
+    let { email, password } = data
 
-        if (!email || !password) {
-                return res.status(400).json({ message: "All the fields are required" })
-            }
-        
-            // Checking user exists
-            const user = await userModel.findOne({ 
-                email 
-            })
-        
-            if (!user) {
-                return res.status(404).json({ message: "User not found" })
-            }
-        
-            let isPasswordValid = bcrypt.compareSync(password, user.password)
-
-            if(!isPasswordValid){
-                return res.status(401).json({ message: "Invalid credentials" })
-            }
-        
-            let accessToken = generateAccessToken(user._id)
-            let refreshToken = generateRefreshToken(user._id)
-
-            return {
-                accessToken, refreshToken, user
-            }
-
-    } catch (error) {
-        throw new Error("Internal Server Error")
+    if (!email || !password) {
+      throw new Error("All fields are required")
     }
+
+    // Checking user exists
+    const user = await userModel.findOne({
+      email,
+    })
+
+    if (!user) {
+      throw new Error("User not found")
+    }
+
+    let isPasswordValid = bcrypt.compareSync(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials")
+    }
+
+    let accessToken = generateAccessToken(user._id)
+    let refreshToken = generateRefreshToken(user._id)
+
+    return {
+      accessToken,
+      refreshToken,
+      user,
+    }
+  } catch (error) {
+    throw new Error(error)
+  }
 }
